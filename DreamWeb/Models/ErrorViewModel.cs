7818fe10b4f0f393e3c76e5f62c810143abcc0ec
everscope <im2423@gmail.com>
2021-12-10 12:32:01 +0200

    first commit

---
+++ DreamWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DreamWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
